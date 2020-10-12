9205c88cbe1639c32c487ff34f206c759decb30e
nutunabe <baiytaan@gmail.com>
2020-10-12 10:19:26 +0900

    lab1 fedorovb

--- myproxy.py
+++ myproxy.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import request
+import requests
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello FIIT-17!'
+@app.route('/', methods=['GET','POST'])
+def get_page():
+    if 'url' in request.args:
+        r = requests.get(request.args['url'])
+        return r.content
+    else:
+        return '''
+        <form method="get" autocomplete="off">
+            <input name="url" type="text">
+            <input type="submit">
+        <form>'''
 
 if __name__ == '__main__':
     app.run()
